2e83f0935910bc925d61fa0d6ea305accfb2071a
NanoProgram <danielj.perezd@gmail.com>
2023-02-18 13:31:53 +0000

    Start

--- src/models.py
+++ src/models.py
@@ -8,12 +8,11 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -23,8 +22,8 @@ class Address(Base):
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)
 
     def to_dict(self):
         return {}
